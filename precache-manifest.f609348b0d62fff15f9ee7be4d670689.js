330770f59054591dcb203261c11fafd7732a0be5
Gleb Matorin <gleb.matorin@gmail.com>
2019-10-23 15:32:01 +0700

    Updates

---
+++ precache-manifest.f609348b0d62fff15f9ee7be4d670689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708f472d7e758a323238442881b1f5dd",
+    "url": "/Image_Finder/index.html"
+  },
+  {
+    "revision": "96c42151e7dd8c744ba6",
+    "url": "/Image_Finder/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "cc20ea949a467a521285",
+    "url": "/Image_Finder/static/css/main.bcb77211.chunk.css"
+  },
+  {
+    "revision": "96c42151e7dd8c744ba6",
+    "url": "/Image_Finder/static/js/2.193fd231.chunk.js"
+  },
+  {
+    "revision": "cc20ea949a467a521285",
+    "url": "/Image_Finder/static/js/main.90a7ad45.chunk.js"
+  },
+  {
+    "revision": "4c1777c206ae24bf7276",
+    "url": "/Image_Finder/static/js/runtime-main.6788235a.js"
+  }
+]);
